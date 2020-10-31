5f0227f8844b3646b4857a2e0809a4833111ee0d
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-10-31 18:00:51 -0400

    feat: cli argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+// std
+
+// crates
+use clap::{App, Arg, SubCommand};
+
+// local
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("halimede")
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("Add a new template")
+                .arg(
+                    Arg::with_name("local")
+                        .short("l")
+                        .help("Add a local directory as a template.")
+                        .takes_value(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Start a new project")
+                .arg(
+                    Arg::with_name("template")
+                        .short("t")
+                        .help("The name of the template to use")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("INPUT")
+                        .help("The output directory")
+                        .default_value(".")
+                        .required(false),
+                ),
+        )
+        .subcommand(SubCommand::with_name("list").about("List all available templates."))
+        .subcommand(
+            SubCommand::with_name("remove")
+                .about("Remove a template")
+                .arg(
+                    Arg::with_name("template")
+                        .short("t")
+                        .required(true)
+                        .help("The name of the template to remove."),
+                ),
+        )
+        .get_matches();
+
+    if let Some(_v) = app.subcommand_matches("add") {
+    } else if let Some(_v) = app.subcommand_matches("init") {
+    } else if let Some(_v) = app.subcommand_matches("list") {
+    } else if let Some(_v) = app.subcommand_matches("remove") {
+    }
 }
